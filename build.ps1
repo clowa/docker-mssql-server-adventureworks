52d774922d80ba21a0ba862761f2a83ec531f96f
Cedric Ahlers <cedric.ahlers@posteo.de>
2021-12-30 10:26:00 +0100

    allow build of just specific versions

--- build.ps1
+++ build.ps1
@@ -1,18 +1,34 @@
-$DockerTag = "clowa/mssql-server-adventureworks2017"
-$Platform = @("linux/amd64")
-$Types = @(
-    "LT",
-    "DW",
-    ""
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [String]
+    $DockerTag = "clowa/mssql-server-adventureworks2017",
+
+    [Parameter()]
+    [String[]]
+    $Platform = @("linux/amd64"),
+
+    [Parameter()]
+    [String[]]
+    $Years = @(
+        "2019",
+        "2017",
+        "2016",
+        "2014"
+        "2012"
+    ),
+
+    [Parameter()]
+    [String[]]
+    [ValidateSet("LT", "DW", "OLTP")]
+    $Types = @(
+        "LT",
+        "DW",
+        "OLTP"
+    )
 )
 
-$Years = @(
-    "2019",
-    "2017",
-    "2016",
-    "2014"
-    "2012"
-)
+$Types[$Types.IndexOf("OLTP")] = ""
 
 [Array]::Sort($Types)
 [Array]::Sort($Years)
@@ -20,10 +36,13 @@ $Years = @(
 foreach ($y in $Years) {
     Write-Host "Processing version of year $y"
     docker manifest inspect "mcr.microsoft.com/mssql/server:$y-latest" *> $null
-    if ($LASTEXITCODE) { continue; }
+    if ($LASTEXITCODE) {
+        Write-Warning "Docker image mcr.microsoft.com/mssql/server:$y-latest does not exist."
+        continue
+    }
 
     foreach ($t in $Types) {
-        Write-Host "Processing $($DockerTag):$y$t"
+        Write-Host "`tProcessing $($DockerTag):$y$t"
         $cmd = "docker buildx build --platform $($Platform -join ',') --tag $($DockerTag):$y$t --build-arg MSSQLYear=$y --build-arg AdventureWorksYear=$y --build-arg AdventureWorksType=$t . --push"
         Invoke-Expression $cmd
         # Start-Job { Invoke-Expression $cmd }
